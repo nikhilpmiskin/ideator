09a26c41d7b295b197b1991704eed38e3eb613f1
nikhilpmiskin <nikhilpmiskin@gmail.com>
2024-09-12 16:45:02 -0700

    add basename

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/ideator">
       <div className="App">
         <Routes>
           {/* <Route path="/" element={<Chat />} /> */}
